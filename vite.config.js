633849b5bfe978813aea3da23e257fda187bc0df
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-11-18 12:30:22 +0500

    deps updated and pnpm applied

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import imagemin from "vite-plugin-imagemin";
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 // https://vitejs.dev/config/
 export default defineConfig({
